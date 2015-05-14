a8fec7c7cc848587751532573ab89fd501855527
BE Design <info@b-ed.com>
2015-05-13 21:01:11 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+	<head>
+		<title></title>
+		<link href='http://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+
+	<body>
+
+		<div id="wrap">
+			 <div id="logo">
+			 	<img src="/images/TZR-logo.jpg" alt="The Zoe Report logo" />
+			 	<h2>shopping experience powered by bringhub</h2>
+			 </div> <!-- END #logo -->
+
+			 <h1>Contact Us</h1>
+
+			 <form action="">
+			 	<div class="left">
+			 		<div class="form-item">
+			 			<label for="">Reason</label>
+				 		<select name="" id="">
+				 			<option value="1">option</option>
+				 			<option value="2">option</option>
+				 			<option value="3">option</option>
+				 			<option value="4">option</option>
+				 		</select>
+			 		</div>
+
+			 		<div class="form-item">
+			 			<label for="">Contact Name*</label>
+			 			<input type="text" />
+			 		</div>
+
+			 		<div class="form-item">
+			 			<label for="">Contact Email*</label>
+			 			<input type="text" />
+			 		</div>
+
+			 		<div class="form-item">
+			 			<label for="">Transaction Number</label>
+			 			<input type="text" />
+			 		</div>
+			 	</div> <!-- END .left -->
+
+			 	<div class="right">
+			 		<div class="form-item">
+				 		<label for="">Notes</label>
+				 		<textarea name="" id="" cols="30" rows="10"></textarea>
+			 		</div>
+			 		<div class="actions">
+			 			<input type="submit" value="submit" />
+			 		</div>
+			 	</div> <!-- END .right -->
+			 </form> <!-- END form -->
+			
+		</div> <!-- END #wrap -->
+
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" ></script>
+		<script type="text/javascript" src="js/jquery.minimalect.min.js"></script>
+		<script type="text/javascript" src="js/init.js"></script>
+
+	</body>
+
+</html>
---
+++ js/init.js
@@ -0,0 +1,15 @@
+
+$( document ).ready(function() {
+    
+// ==============================================================
+// Select tweaks
+// ==============================================================
+    
+    $("select").minimalect();
+
+    $('.minict_wrapper').append('<div class="arrow"><div class="icon-arrow-down" /></div>');
+
+
+
+});
+
